a848d0e0b05e71ef96fd37f04434bc808be6005b
tkgs16 <160175519+tkgs16@users.noreply.github.com>
2024-03-20 23:07:17 +0200

    enforce chat with token

--- src/routes/messages.route.ts
+++ src/routes/messages.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { addMessage, getMessages } from "../controllers/message.controller";
+import authMiddleware from "../common/auth.middleware";
 const router = express.Router();
 
 
 
 
-router.post("/addMsg", addMessage);
-router.post("/getMsg", getMessages);
+router.post("/addMsg",authMiddleware, addMessage);
+router.post("/getMsg",authMiddleware, getMessages);
 
 export default router;
